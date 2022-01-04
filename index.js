1de655e8ee1014cff4243b494480d8675e80d170
Jonathan Moser <jonathancmoser@gmail.com>
2022-01-04 18:43:57 -0500

    updated index

---
+++ index.js
@@ -0,0 +1,14 @@
+const generateHTML = require('./src/generate-html');
+const { writeFile, copyFile } = require('./utl/write-and-copy-file');
+const { promptManager, promptTeam } = require('./utl/prompt-user');
+
+promptManager()
+  .then(teamData => promptTeam(teamData))
+  .then(teamData => generateHTML(teamData))
+  .then(pageHTML => writeFile(pageHTML))
+  .then(writeResponse => {
+    console.log(writeResponse);
+    return copyFile();
+  })
+  .then(copyResponse => console.log(copyResponse))
+  .catch(err => console.log(err));
